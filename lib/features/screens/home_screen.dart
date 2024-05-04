60cfd8e33bd2f2d7b05ddcccccf6ed48d9feedbf
Franco Galbiati <francogalbiati984@gmail.com>
2024-05-04 14:54:28 -0500

    add links linkedin and github

--- lib/features/screens/home_screen.dart
+++ lib/features/screens/home_screen.dart
@@ -1,8 +1,8 @@
-// ignore_for_file: deprecated_member_use
-import 'package:cv_online/config/router/app_router.dart';
 import 'package:cv_online/features/infrastructure/provider/theme_app_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
@@ -11,26 +11,7 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isDarkMode = ref.watch(darkModeProvider);
     return Scaffold(
-      drawer: NavigationDrawer(children: [
-        const DrawerHeader(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Informacion de contacto'),
-              SizedBox(
-                height: 20,
-              ),
-              Text('francogalbiati984@gmail.com'),
-            ],
-          ),
-        ),
-        ListTile(
-          title: const Text('Details'),
-          onTap: () {
-            ref.read(appRouterProvider).go('/details');
-          },
-        ),
-      ]),
+      drawer: const AppDrawer(),
       appBar: AppBar(
         backgroundColor: isDarkMode == false
             ? const Color.fromARGB(255, 79, 164, 238)
@@ -48,5 +29,71 @@ class HomeScreen extends ConsumerWidget {
         ],
       ),
     );
+  }
+}
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationDrawer(children: [
+      const DrawerHeader(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Informacion de contacto'),
+            SizedBox(
+              height: 20,
+            ),
+            Text('francogalbiati984@gmail.com'),
+            SizedBox(
+              height: 10,
+            ),
+            Text('+54 9 11 62271-014'),
+          ],
+        ),
+      ),
+      TextButton(
+        onPressed: () =>
+            _launchUrl('https://www.linkedin.com/in/franco-galbiati/'),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Ionicons.logo_linkedin),
+            SizedBox(
+              width: 10,
+            ),
+            Text('Linkedin'),
+          ],
+        ),
+      ),
+      const SizedBox(
+        height: 10,
+      ),
+      TextButton(
+        onPressed: () => _launchUrl('https://github.com/Galbfran'),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Ionicons.logo_github),
+            SizedBox(
+              width: 10,
+            ),
+            Text('Github'),
+          ],
+        ),
+      )
+    ]);
+  }
+}
+
+Future<void> _launchUrl(String url) async {
+  final Uri url0 = Uri.parse(url);
+  if (await launchUrl(url0)) {
+    throw Exception('Could not launch $url0');
   }
 }
